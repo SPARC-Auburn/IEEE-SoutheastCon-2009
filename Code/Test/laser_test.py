93409eb0403327c962217bde0159a1dd8594e0f2
William Woodall <wjwwood@gmail.com>
2008-09-06 03:42:13 +0000

    Added another test to laser_test.py in the Test directory.

--- Code/Test/laser_test.py
+++ Code/Test/laser_test.py
@@ -1,12 +1,35 @@
 import serial
 
-sio = serial.Serial("/dev/ttyACM0")
-sio.timeout = 2
-sio.write("G00076800\r")
 
-line = sio.readline()
-while (line != ''):
-	print line
+
+
+
+
+def simple(self):
+	sio = serial.Serial("/dev/ttyACM0")
+	sio.timeout = 2
+	sio.write("G00076800\r")
+	
 	line = sio.readline()
+	while (line != ''):
+		print line
+		line = sio.readline()
+	
+	sio.close()
 
-sio.close()
+
+def trimmed():
+	sio = serial.Serial("/dev/ttyACM0")
+	sio.timeout = 2
+
+	for i in range(0,10):
+		print i,
+		sio.write("G00076800\r")
+		
+		sio.readline()
+		sio.readline()
+		
+		data = sio.read(1564)
+		sio.flushInput()
+
+trimmed()
