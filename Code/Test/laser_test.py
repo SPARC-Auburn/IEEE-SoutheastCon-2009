775cfc4f003dc53d7dd114fc6c369d019cfbe73c
William Woodall <wjwwood@gmail.com>
2008-09-16 21:27:20 +0000

    Added some iterative testing to laser_test.py, I think it is going to be fast enough...

--- Code/Test/laser_test.py
+++ Code/Test/laser_test.py
@@ -1,5 +1,7 @@
-import serial
+# ! /usr/bin/python
 
+import serial
+import time
 
 
 
@@ -36,22 +38,29 @@ def granual():
 	sio = serial.Serial("/dev/ttyACM0")
 	sio.timeout = 2
 	
-	sio.write("G00076800\r")
-	sio.readline()
-	sio.readline()
-	
-	line1 = sio.read()
-	line2 = sio.read()
-	while((line1 != '\n') or (line2 != '\n')):
-		if(not((line1 == '0') and (line2 == 'C'))):
-			if(line1 != '\n'):
-				print line1,
-			if(line2 != '\n'):
-				print line2,
-			print ' ',
+	for x in range(0,10):	
+		sio.write("G00076800\r")
+		sio.readline()
+		sio.readline()
+		time1 = time.time()
 		line1 = sio.read()
 		line2 = sio.read()
+		while((line1 != '\n') or (line2 != '\n')):
+			if(not((line1 == '0') and (line2 == 'C'))):
+				if(line1 != '\n'):
+#					print line1,
+					pass
+				if(line2 != '\n'):
+#					print line2,
+					pass
+#				print ' ',
+				pass
+			line1 = sio.read()
+			line2 = sio.read()
+#		print
+		print "Test",x,":",(time.time()-time1)*1000,"milliseconds."
 	sio.close()
 
 #trimmed()
 granual()
+#simple()
