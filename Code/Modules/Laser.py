c0016990d99df0a78bfeaa9a654f9869bdf19123
William Woodall <wjwwood@gmail.com>
2008-09-23 14:48:25 +0000

    Added documentation and I am changing the capitalization of the module/class.

--- Code/Modules/Laser.py
+++ Code/Modules/Laser.py
@@ -7,8 +7,14 @@
 #
 
 
-import pyserial
+import serial
 
-class laser(serial.Serial):
+class Laser(serial.Serial):
+	"""Laser class, extends serial so you can use it like a serial port.
+	
+	The Laser class extends the serial class so you can create it and pass it the serial port to it at the same time. i.e.:
+	laser1 = laser.Laser("/dev/ttyACM0")
+	You can perform any function on it that you can perform on a serial object as well as, scan()."""
 	def scan(self, start = "000", finish = "768", step = "00"):
+		# Check input
 		
