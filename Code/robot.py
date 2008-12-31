13f26f3e28cae995a5e8f3cc11c4d68bdfe9b67d
William Woodall <wjwwood@gmail.com>
2008-12-31 06:04:01 +0000

    Added Several new folders in preperation for adding configurations.

--- Code/robot.py
+++ Code/robot.py
@@ -5,3 +5,34 @@
 #  Created by William Woodall on 12/4/08.
 #  Copyright (c) 2008 Auburn University. All rights reserved.
 #
+
+# Paths #
+import sys
+sys.path.append("Libraries")
+sys.path.append("Modules")
+
+# Imports #
+# ConfigObj
+from configobj import ConfigObj
+from validate import Validator
+# Logging
+#from logger import *  # Logging is not working yet.
+
+# Robot #
+class Robot:
+	pass
+
+# Functions #
+
+
+
+
+if __name__ == "__main__":
+	# Handle Configurations
+	try:
+		filename == None
+	except NameError:
+		filename = "Configurations/robot.cfg"
+	else:
+		print "Using non default config file:", filename
+	config = ConfigObj(filename)
