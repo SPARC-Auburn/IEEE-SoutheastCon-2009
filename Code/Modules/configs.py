051ed9b9ebebfa8b55de3e055f23f96d940e1d97
William Woodall <wjwwood@gmail.com>
2009-01-27 21:07:07 +0000

    Forgot I changed the name of the config module to configs so had to del and add the olad and new files.

--- Code/Modules/configs.py
+++ Code/Modules/configs.py
@@ -14,10 +14,20 @@ from validate import Validator
 
 global config
 
+def convert_bools(s, k):
+	if s[k] == 'True' or s[k] == 'true' or s[k] == '1':
+		s[k] = True
+	if s[k] == 'False' or s[k] == 'false' or s[k] == '0':
+		s[k] = False
+	
 def init(configFile):
 	global config
 	config = ConfigObj(configFile)
-
-def get_config(sectionKey):
+	config.walk(convert_bools)
+	
+def get_config(sectionKey = ''):
 	global config
-	return config[sectionKey]
+	if sectionKey is '':
+		return config
+	else:
+		return config[sectionKey]
