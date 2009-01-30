f2ebd8815bd153bcdce4796e27be459efe816108
William Woodall <wjwwood@gmail.com>
2009-01-30 19:30:41 +0000

    Forgot to svn add the modules doh

---
+++ Code/Modules/micro_controller_network.py
@@ -0,0 +1,93 @@
+#
+#  micro_controller_network.py
+#  
+#
+#  Created by William Woodall on 1/29/09.
+#  Copyright (c) 2009 Auburn University. All rights reserved.
+#
+'''
+	Module for interfacing with the Micro Controller Network.
+	TODO:
+	* Controlling lights and switches
+	* Handleing Interrupts from Master Node.
+	'''
+
+# Imports #
+# Logging
+import logging
+log = logging.getLogger('MCN')
+master_log = logging.getLogger('Master Node')
+# Configs
+import configs
+global config, enable
+config = configs.get_config('Micro Controller Network')
+enabled = config['enabled']
+# PySerial
+if enabled:
+	try:
+		from serial import Serial
+	except Exception as e:
+		log.error("Serial cannot be imported, you may need to install it: %s" % e)
+else:
+	log.warning("The Micro Controller Network is Disabled and all connect Equiptment will not work!")
+
+# Static Functions #
+def init():
+	global config, initialized, master_nodes
+	master_nodes = {}
+	master_nodes['master'] = MasterNode(serial = config['serial_port'])
+	initialized = True
+	
+def get_object(id = 'master'):
+	global master_nodes, initialized
+	if not initialized:
+		log.critial("The micro_controller_network.init() method has to be called before retrieving objects.")
+		exit(1)
+	try:
+		return master_nodes[id]
+	except KeyError:
+		log.error("You provided an invalid id for any of the available Master Nodes.  Please double check the names in the config files.")
+		
+# Classes #
+class MasterNode:
+	'''
+		This is the class that represents and provides access to the master 
+		node of a micro controller network.
+		'''
+	def __init__(self, serial=''):
+		'''
+			Constructor - opens serial port given and initializes the master node.
+			'''
+		global enabled
+		if enabled:
+			try:
+				self.serial = Serial(serial)
+			except Exception as e:
+				log.error("Unable to open serial port %s: %s", (serial, e))
+		return
+	
+	def send(address, msg):
+		''''
+			Sends a msg to a micro controller at the given address.
+			Address must be a byte.
+			msg must be a list of bytes to send.
+			Bytes must be a 2 char string representing two hex digits.
+			'''
+		# Checks
+		if not enabled:
+			return
+		if type(msg) is not list:
+			log.error("Messages sent to the master node must be a list of bytes!")
+			return
+		
+		# Implementation
+		message = '['
+		message += '0h'
+		message += address
+		for x in msg:
+			message += ' 0h'
+			message += x
+		message += ']'
+		master_log.debug("Sending message: %s", message)
+		self.serial.write(message)
+		return
---
+++ Code/Modules/servo_controller.py
@@ -0,0 +1,114 @@
+#
+#  servo_controller.py
+#  
+#
+#  Created by William Woodall on 1/29/09.
+#  Copyright (c) 2009 Auburn University. All rights reserved.
+#
+'''
+	Module for communicating with the servo controller.
+	'''
+
+# Imports #
+# Logging
+import logging
+log = logging.getLogger('Servo Ctrl')
+# Configs
+import configs
+global config, enable
+config = configs.get_config('Servo Controller')
+enabled = config['enabled']
+# micro_controller_network
+import micro_controller_network
+
+# Static Functions #
+def init():
+	global config, initialized, servos, address, mcn
+	servos = {}
+	for x in config:
+		if type(config[x]) is list:
+			servos[x] = Servo(config = config[x])
+	address = config['address']
+	mcn = micro_controller_network.get_object()
+	initialized = True
+	
+def get_object(id):
+	global servos, initialized
+	if not initialized:
+		log.critial("The servo_controller.init() method has to be called before retrieving objects.")
+		exit(1)
+	try:
+		return servos[id]
+	except KeyError:
+		log.error("You provided an invalid id for any of the available servos.  Please double check the names in the config files.")
+		
+# Classes #
+class Servo:
+	'''
+		This class represents and allows control of a servo controlled by 
+		a servo controller on a micro controller network.
+		'''
+	def __init___(self, pointer = None, zero = 1500, max = 2400, min = 600, config = None):
+		'''
+			Constructor - Creates a servo obejct given a configuration list.
+			'''
+		global config
+		if config is None and pointer is None:
+			log.error("When creating a servo either pointer or config must be defined, but not both.")
+		if config is not None and pointer is not None:
+			log.error("When creating a servo either pointer or config must be defined, but not both.")
+		if config is None:
+			self.pointer = str(pointer)
+			self.zero = zero
+			self.max = max
+			self.min = min
+		else:
+			self.pointer = config['pointer']
+			self.zero = int(config['zero'])
+			self.max = int(config['max'])
+			self.min = int(config['min'])
+		return
+				
+	def move_to_position(position = 1500):
+		'''
+			Moves the servo to the given position.
+			'''
+		global enabled, mcn, address
+		if not enabled:
+			return
+		position = '%X' % position
+		if len(position) % 2 is 1:
+			position = '0' + position
+		msg = []
+		if len(pointer) <= 1:
+			pointer = '0' + pointer
+		msg.append(pointer)
+		for x in range(len(position)):
+			if x % 2 is 1:
+				msg.append(position[x:x+2])
+		mcn.send(address, msg)
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		a
