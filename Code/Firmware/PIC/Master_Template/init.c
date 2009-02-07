e5e544eaa1a2cbc1c108f0d7a2eb818f994cf95e
Michael Carroll <carroll.michael@gmail.com>
2009-02-07 18:16:31 +0000

    Master Template work.
    Also, schematics for part of our microcontroller circuits.

--- Code/Firmware/PIC/Master_Template/init.c
+++ Code/Firmware/PIC/Master_Template/init.c
@@ -6,7 +6,7 @@
 int Init (void) 
 {	
 	Init_Oscillator();
-//	Init_Interrupts();
+	Init_Interrupts();
 	Init_I2C();
 	Init_USART();
 	
@@ -18,6 +18,7 @@ int Init (void)
 void Init_Oscillator(void)
 {
 	OSCCON = 0b01110000; //configure PIC to primary oscillator block at 8MHz
+	OSCTUNEbits.PLLEN = 1;
 }
 
 void Init_Interrupts(void)
@@ -32,20 +33,20 @@ void Init_I2C(void)
 	OpenI2C(MASTER,SLEW_OFF);
 	// In Master Mode:
 	// Clock = Fosc/(4 * (SSPADD + 1)
-	// SSPADD = Fosc/(4 * Fi2c) - 1 = 8meg/4*100k - 1 = 19
-	// 19 = 0x13
-	// SSPADD = 19;
-	SSPADD = 27; //This is so I can actually see stuff on my old O-Scope
+	// SSPADD = Fosc/(4 * Fi2c) - 1 = 32meg/4*100k - 1 = 79
+	SSPADD = 79;
 }
 
 void Init_USART(void)
 {
-	int baud = 25;
+	int baud = 68;
 	
 	TXSTA = 0;		// Reset registers
 	RCSTA = 0;
 	RCSTAbits.CREN = 1;		// Continuous Reception
 	TXSTAbits.BRGH = 1;		// High Baud Rate
+	BAUDCONbits.BRG16 = 1;
+	
 	PIR1bits.TXIF = 0;
 	PIE1bits.RCIE = 0;		// No Receive Interrupt
 	PIR1bits.RCIF = 0;
