d796f7839245586dacb882820bef7efdcc877848
Michael Carroll <carroll.michael@gmail.com>
2009-02-19 17:06:57 +0000

    Tweaked a few things in the object detection code.
    Renamed to follow conventions.

--- Code/Firmware/PIC/OBJ/main.c
+++ Code/Firmware/PIC/OBJ/main.c
@@ -86,6 +86,7 @@ void main (void)
 //                          setup
 //***************************************************************************************************************
 	Init();	
+	initQueue();
 
 	TRISAbits.TRISA4 = 1;		// set pin 6 as input for microswitch
 	
