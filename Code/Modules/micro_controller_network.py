ebca6d1bb0be5404282be4e6542dbba1b9d56eeb
William Woodall <wjwwood@gmail.com>
2009-02-19 01:28:41 +0000

    Fixed an error in the move_to_position command.

--- Code/Modules/micro_controller_network.py
+++ Code/Modules/micro_controller_network.py
@@ -98,7 +98,7 @@ class MicroController:
 		for x in msg:
 			message += x
 		self.send_lock.acquire()
-		master_log.debug("Sending message: %s" % message)
+		master_log.debug("Sending message: %s" % hex.encode(message))
 		if self.serial.isOpen():
 			self.serial.write(message+'\r')
 		self.send_lock.release()
