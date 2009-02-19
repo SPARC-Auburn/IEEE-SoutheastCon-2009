197f4fcc890873fa0f462fa5f1749ad7d495bbe4
Sam Misko <sam.misko@gmail.com>
2009-02-19 19:23:13 +0000

    UPDATED ARRAY

--- Code/Firmware/PIC/ARRAY/op_codes.h
+++ Code/Firmware/PIC/ARRAY/op_codes.h
@@ -1,21 +1,21 @@
-#define 	RESET_OP				0x00
-#define 	EEPROM_WR_OP			0x01
-#define		EEPROM_RD_OP			0x02
-#define		CAL_ANT_OP				0x10
-#define		LINE_FOLLOW_OP			0x11
-#define		CORNER_DETECTION_OP		0x12
-#define		LINE_DETECTION_OP		0x13
+#define 	RESET_OP				0x30
+#define 	EEPROM_WR_OP			0x31
+#define		EEPROM_RD_OP			0x32
+#define		CAL_ANT_OP				0x40
+#define		LINE_FOLLOW_OP			0x41
+#define		CORNER_DETECTION_OP		0x42
+#define		LINE_DETECTION_OP		0x43
 
-#define		INT_CAL_ANT_DONE		0x10	// Antenna Calibration Complete
+#define		INT_CAL_ANT_DONE		0x40	// Antenna Calibration Complete
 
-#define		INT_LINE_LEFT			0x11
-#define		INT_LINE_RIGHT			0x12
-#define		INT_LINE_CENTER			0x13
-#define		INT_LINE_ERROR			0x19
+#define		INT_LINE_LEFT			0x41
+#define		INT_LINE_RIGHT			0x42
+#define		INT_LINE_CENTER			0x43
+#define		INT_LINE_ERROR			0x49
 
-#define		INT_CORNER_DETECT		0x14
-#define		INT_CORNER_NO_DETECT	0x15
+#define		INT_CORNER_DETECT		0x44
+#define		INT_CORNER_NO_DETECT	0x45
 
-#define		INT_LINE_DETECT_1		0x16
-#define		INT_LINE_DETECT_2		0x17
-#define		INT_LINE_DETECT_3		0x18
+#define		INT_LINE_DETECT_1		0x46
+#define		INT_LINE_DETECT_2		0x47
+#define		INT_LINE_DETECT_3		0x48
