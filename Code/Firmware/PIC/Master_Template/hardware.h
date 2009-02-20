bfa4ce205d2c0bd85e0183f84aec4e3189df6fb3
Sam Misko <sam.misko@gmail.com>
2009-02-20 17:47:21 +0000

    Master_Template gone.

--- Code/Firmware/PIC/Master_Template/hardware.h
+++
@@ -1,7 +0,0 @@
-#ifndef HARDWARE_H
-#define HARDWARE_H
-
-#include <p18cxxx.h>
-#include <i2c.h>
-
-#endif
--- Code/Firmware/PIC/Master_Template/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-void	high_isr 		(void);
-void	high_vec 		(void);
-void 	low_vec  		(void);
-void 	low_isr  		(void);
-
-#endif
