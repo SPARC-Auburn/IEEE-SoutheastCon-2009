b4edd9a5b63b4bec08385902c05385bed266016d
William Woodall <wjwwood@gmail.com>
2009-02-21 17:03:50 +0000

    Updated to maybe allow remote control driving and calling other functions.

--- Code/Utilities/Remote Client/remote_server.py
+++ Code/Utilities/Remote Client/remote_server.py
@@ -10,6 +10,8 @@ os.chdir('../../')
 import sys
 sys.path.append('./')
 
+from threading import Thread
+
 from robot import *
 
 from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
@@ -25,52 +27,59 @@ soc.bind( (hostName, PORT_NUMBER) )
 info("Server started on port: %i" % PORT_NUMBER)
 
 # Loop
-def loop():
-	arm = 0.0
-	arm_bool = True
-	sorter = 0.0
-	sorter_bool = True
-	gripper = 0.0
-	gripper_bool = True
-	while True:
-		(data, addr) = soc.recvfrom( 1024 )
-		debug("Recieved Pack from %s: %s" % (addr, data))
-		p = data.partition(' ')
-		speed = float(p[0])
-		p = p[2].partition(' ')
-		direction = float(p[0])
+class loop(Thread):
+	def __init__(self):
+		Thread.__init__(self)
+		self.stop = False
 		
-		p = p[2].partition(' ')
-		if abs(arm - float(p[0])) > SENSITIVITY:
-			arm_bool = True
-			arm = float(p[0])
-		
-		p = p[2].partition(' ')
-		if abs(sorter - float(p[0])) > SENSITIVITY:
-			sorter_bool = True
-			sorter = float(p[0])
-		
-		p = p[2].partition(' ')
-		if abs(gripper - float(p[0])) > SENSITIVITY:
-			gripper_bool = True
-			gripper = float(p[0])
-		
-		move(speed, direction)
-		if arm_bool:
-			arm_servo.move(arm)
-			arm_bool = False
-		if sorter_bool:
-			sorter_servo.move(sorter)
-			sorter_bool = False
-		if gripper_bool:
-			gripper_servo.move(gripper)
-			gripper_bool = False
-		
+	def run(self):	
+		arm = 0.0
+		arm_bool = True
+		sorter = 0.0
+		sorter_bool = True
+		gripper = 0.0
+		gripper_bool = True
+		while not self.stop:
+			(data, addr) = soc.recvfrom( 1024 )
+			debug("Recieved Pack from %s: %s" % (addr, data))
+			p = data.partition(' ')
+			speed = float(p[0])
+			p = p[2].partition(' ')
+			direction = float(p[0])
+			
+			p = p[2].partition(' ')
+			if abs(arm - float(p[0])) > SENSITIVITY:
+				arm_bool = True
+				arm = float(p[0])
+			
+			p = p[2].partition(' ')
+			if abs(sorter - float(p[0])) > SENSITIVITY:
+				sorter_bool = True
+				sorter = float(p[0])
+			
+			p = p[2].partition(' ')
+			if abs(gripper - float(p[0])) > SENSITIVITY:
+				gripper_bool = True
+				gripper = float(p[0])
+			
+			move(speed, direction)
+			if arm_bool:
+				arm_servo.move(arm)
+				arm_bool = False
+			if sorter_bool:
+				sorter_servo.move(sorter)
+				sorter_bool = False
+			if gripper_bool:
+				gripper_servo.move(gripper)
+				gripper_bool = False
+				
+	def shutdown(self):
+		self.stop = True
+		return
+
 try:
-	loop()
+	t = loop()
+	t.start()
 except KeyboardInterrupt:
 	print
 	info("Shutting Down.")
-finally:
-	shutdown()
-	soc.close()
