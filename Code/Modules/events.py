dffc99434e805bad5f079076cb1535729c6f9a4e
William Woodall <wjwwood@gmail.com>
2009-02-26 20:50:39 +0000

    Changed events to use a sequential locking mechanism rather than a threaded polling approach.

--- Code/Modules/events.py
+++ Code/Modules/events.py
@@ -5,53 +5,44 @@
 #  Created by William Woodall on 2/19/09.
 #  Copyright (c) 2009 Auburn University. All rights reserved.
 #
-from threading import Thread, Event
+from threading import Thread, Event, Lock
 from Queue import Queue
 
-lock = Event()
+import logging
+log = logging.getLogger('Events')
 
+event = Event()
+lock = Lock()
 queue = Queue(1024)
 
 def push(e, msg):
+	lock.acquire()
 	if not queue.full():
 		queue.put((e,msg))
+		event.set()
+	else:
+		log.error("Event Queue is FULL!, dropping event %s %s" % (e, msg))
+	lock.release()
 
 def pop():
+	temp = (None, None)
+	lock.acquire()
 	if not queue.empty():
 		temp = queue.get()
-		lock.clear()
-		return temp
+		event.clear()
+		if not queue.empty():
+			event.set()
+	else:
+		log.error("Tried to pop when the queue was empty?  Check your logic, no event has happened, also check for race conditions.")
+	lock.release()
+	return temp
 		
 def triggerEvent(return_code, msg):
-	if not t.stop:
-		push(return_code, msg)
+	push(return_code, msg)
 	
 def wait_for_event(timeout = 1):
-	while not lock.isSet():
-		lock.wait(timeout)
+	while not event.isSet():
+		event.wait(timeout)
 		
 def get_last_event():
 	return pop()
-
-def shutdown():
-	t.shutdown()
-	t.join()
-
-class Notifier(Thread):
-	def __init__(self):
-		Thread.__init__(self)
-		self.stop = False
-		
-	def run(self):
-		while not self.stop:
-			if not queue.empty() and not lock.isSet():
-				lock.set()
-			
-	def shutdown(self):
-		self.stop = True
-		while not queue.empty():
-			queue.get()
-		return
-		
-t = Notifier()
-t.start()
