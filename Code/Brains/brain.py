b6963f7ddeebe1d04ca6e9cbc3fe8d6b178909ec
William Woodall <wjwwood@gmail.com>
2009-02-26 04:38:44 +0000

    Layed out the skeleton for Brainnnnnnss... ARG... Braaaainnnnns...

---
+++ Code/Brains/brain.py
@@ -0,0 +1,11 @@
+#
+#  brain.py
+#  
+#
+#  Created by William Woodall on 2/25/09.
+#  Copyright (c) 2009 Auburn University. All rights reserved.
+#
+import sys
+sys.path.append('../')
+sys.path.append('../Modules')
+sys.path.append('../Libraries')
---
+++ Code/Brains/line_follow_brain.py
@@ -0,0 +1,13 @@
+#
+#  line_follow_brain.py
+#  
+#
+#  Created by William Woodall on 2/25/09.
+#  Copyright (c) 2009 Auburn University. All rights reserved.
+#
+
+import brain
+from robot import *
+from time import sleep
+
+# Control Code #
