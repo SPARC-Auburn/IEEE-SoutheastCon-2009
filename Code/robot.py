34889cbb45a00225a0efa1aff4e9261b46b1fed4
William Woodall <wjwwood@gmail.com>
2009-03-03 03:36:17 +0000

    Propagation disabled for dash_log, again...

--- Code/robot.py
+++ Code/robot.py
@@ -193,7 +193,7 @@ logging.config.fileConfig(loggingConfigFile)
 log = logging.getLogger('Robot')
 brain_log = logging.getLogger('Brain')
 dash_log = logging.getLogger('Dash_Log')
-#dash_log.propagate = False
+dash_log.propagate = False
 serverHandler = LoggingServerHandler('', PORT)
 dash_log.addHandler(serverHandler)
 log.debug("Using logging config file %s" % loggingConfigFile.name)
